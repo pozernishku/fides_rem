8d0d21758a2e264ec8380f03dbb21940a78f7514
Anton <pozernishku@gmail.com>
2019-09-04 04:17:51 +0300

    550

--- WARC.py
+++ WARC.py
@@ -523,7 +523,7 @@ def clean_tokenize_frqdis_wet_files(wet_list=None, done_list_file='wet.paths.don
         return
     
 #     wet_list = wet_list[-2:-1] # one (last 00639) in list (require all list)
-    wet_list = wet_list[280:350] #!!!!
+    wet_list = wet_list[350:550] #!!!!
     
     for wet_file in wet_list:
         # new iteration if wet_file is done earlier
